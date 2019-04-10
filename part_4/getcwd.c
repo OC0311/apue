8c8b97fcb8d23a44a10a6df9e6d7757f45091b5d
rjj <rjj@rjjdeMacBook-Air.local>
2019-04-10 22:45:09 +0800

    feat#add getcwd

---
+++ part_4/getcwd.c
@@ -0,0 +1,30 @@
+#include "apue.h"
+
+char* path_allocv2(int* size)
+{
+    char *p = NULL;
+    if(!size) return NULL;
+        p = malloc(256);
+    if(p)
+        *size = 256;
+    else
+        *size = 0;  
+    return p;
+}
+
+int main(void){
+
+    char *ptr;
+    int size;
+    if (chdir("/Users/rjj/learn/apue/part_3") <0 ){
+        err_sys("chdir failed!");
+    }
+    ptr = path_allocv2(&size);
+    if (getcwd(ptr,size) == NULL){
+        err_sys("getcwd failed");
+
+    }
+
+    printf("cwd = %s\n",ptr);
+    exit(0);
+}
