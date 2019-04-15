7b1a856f83fbd75988d11588ff56097cc2f17d74
rjj <rjj@rjjdeMacBook-Air.local>
2019-04-15 22:35:48 +0800

    feat#add procese

---
+++ part_7/atexit.c
@@ -0,0 +1,25 @@
+#include"apue.h"
+#include<stdlib.h>
+
+void exit1(void){
+    printf("call exit1..... \n");
+}
+
+void exit2(void){
+    printf("call exit2..... \n");
+}
+
+
+int main(void){
+    // 顺序相反  估计使用栈实现的
+    // 可以被登记多次
+    if (atexit(exit1) <0){
+        err_sys("call exit1 error");
+    }
+    if (atexit(exit1) <0){
+        err_sys("call exit1 error");
+    }
+    if ((atexit(exit2) <0)){
+        err_sys("call exit2 error");
+    }
+}
---
+++ part_7/hello.c
@@ -0,0 +1,7 @@
+#include "apue.h"
+
+ int main(void){
+     // 编译执行后
+     // 使用echo $? 可以看到终止码
+    return 12;
+}
