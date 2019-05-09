fe6cd6a9d3a2c5108b6e0e97d568e2a0f4f8b41c
jiangjincc <raojiangjin@gmail.com>
2019-05-09 23:16:21 +0800

    ADD#my_alarm

---
+++ part_10/my_alarm.c
@@ -0,0 +1,29 @@
+#include "apue.h"
+#include <pwd.h>
+
+static void my_alarm(int signo){
+
+    struct passwd *rootptr;
+    printf("in signal handler \n");
+    if ((rootptr = getpwnam("root")) == NULL){
+        err_sys("getpwam(root) error");
+
+    }
+    alarm(1);
+
+}
+
+int main(void){
+    struct passwd *ptr;
+    // 注册信号处理函数
+    signal(SIGALRM,my_alarm);
+    for (;;){
+        // 需要设置为自己用的户名
+        if((ptr = getpwnam("rjj")) == NULL){
+            err_sys("getpwnam error");
+        }
+        if(strcmp(ptr->pw_name,"rjj") !=0 ){
+            printf("return value corrupted ! pw_name=%s\n",ptr->pw_name);
+        }
+    }
+}
