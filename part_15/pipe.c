7070a24197f8c72ab26373246d140a86698fb7ae
jiangjincc <raojiangjin@gmail.com>
2019-05-27 23:36:18 +0800

    feat:my popen

--- part_15/pipe.c
+++ part_15/pipe.c
@@ -14,11 +14,15 @@ int main(void){
         err_sys("fork error");
     }else if(pid > 0){
         // 关闭读取的？？？
-        // close(fd[0]);
+        // 因为子进程会复制文件描述符
+        // 每个文件描述符都会有两个连接
+        // 所以父进程先关闭fd[0]
+        // 子进程需要关闭fd[1]
+        close(fd[0]);
         write(fd[1],"hello world\n",12);
     }else{
     
-        // close(fd[1]);
+        close(fd[1]);
         n = read(fd[0], line, MAXLINE);
         write(STDOUT_FILENO,line, n);
     }
